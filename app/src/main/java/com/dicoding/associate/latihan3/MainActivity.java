1c54f29417bc050489c9f84ad4175abf33d1b0b3
MikhwanNugraha <mikhwannugraha@yahoo.co.id>
2018-09-25 08:53:17 +0400

    versi 1.0.0

---
+++ app/src/main/java/com/dicoding/associate/latihan3/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dicoding.associate.latihan3;
+import android.os.Bundle;
+
+public class MainActivity{
+}
---
+++ app/src/main/java/com/dicoding/associate/latihan3/latihansaurangan.java
@@ -0,0 +1,13 @@
+package com.dicoding.associate.latihan3;
+
+public class latihansaurangan {
+
+    public static void main (String [] args){
+        latihansaurangan adin = new latihansaurangan();
+        adin.berjalan();
+    }
+
+    void berjalan(){
+        System.out.println("makan");
+    }
+}
